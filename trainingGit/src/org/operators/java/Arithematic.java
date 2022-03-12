6dfbed840538b3e7c47b68b223524f08dc23eb12
kailashtechouts <kailash.c@techouts.com>
2022-03-12 15:31:50 +0530

    operators example

---
+++ trainingGit/src/org/operators/java/Arithematic.java
@@ -0,0 +1,24 @@
+package org.operators.java;
+
+public class Arithematic {
+
+	public static void main(String[] args) {
+		
+		int a, b, c;
+		a = 8;
+		b =4;
+		c = a + b;
+		 int d = 30 - c; 
+		 int e = d * a;
+		 int f = a / b;
+		 int g = d % c;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+
+	}
+
+}
---
+++ trainingGit/src/org/operators/java/Logical.java
@@ -0,0 +1,22 @@
+package org.operators.java;
+
+public class Logical {
+
+	public static void main(String[] args) {
+		
+		int x = 300;
+		int y = 100;
+		int z = 170;
+		if(x>y && y<z) {
+			System.out.println("hi");
+		}
+		if(z==x || z==y) {
+			System.out.println("how r u you");
+		}
+		if(x!=y && x>=y) {
+			System.out.println("what r u doing");
+		}
+
+	}
+
+}
