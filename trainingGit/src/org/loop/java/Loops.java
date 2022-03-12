8c4caa64ffe99266425df20d0f41d08b8179798a
kailashtechouts <kailash.c@techouts.com>
2022-03-12 15:27:31 +0530

    loop example

---
+++ trainingGit/src/org/loop/java/Loops.java
@@ -0,0 +1,27 @@
+package org.loop.java;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		while (i <= 8) {
+			System.out.println("The value i: " + i);
+			
+			i++;
+		}
+		for(i = 1; i <= 10; i++) {
+			System.out.println("the number are: " +i);
+			
+		}
+		int x = 5;
+		do {
+		     
+		    System.out.println("the number to columns: " +x);
+		    x++;
+		    }
+		    while(x <= 7);
+	}
+}
+
+
