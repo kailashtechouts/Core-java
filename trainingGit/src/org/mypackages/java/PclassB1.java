f39ff8b6e845164a638e140384895e7ed0767ff5
kailashtechouts <kailash.c@techouts.com>
2022-03-12 15:30:15 +0530

    mypackages example

---
+++ trainingGit/src/org/mypackages/java/PclassB1.java
@@ -0,0 +1,12 @@
+package org.mypackages.java;
+
+import org.packages.java.*;
+
+public class PclassB1 {
+	
+ public static void main(String args[]) {
+	 
+	Book bk = new Book();
+	 bk.study();
+ }
+}
