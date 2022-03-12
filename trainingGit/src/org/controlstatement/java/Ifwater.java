e8f6375513ec1fd1429e9e414c9af445d2ae6241
kailashtechouts <kailash.c@techouts.com>
2022-03-12 15:19:34 +0530

    controlstatement example

---
+++ trainingGit/src/org/controlstatement/java/Ifwater.java
@@ -0,0 +1,27 @@
+package org.controlstatement.java;
+
+public class Ifwater {
+
+	public static void main(String[] args) {
+		
+		int i = 16;
+		if(i>18)
+			System.out.println("16 is less than 18");
+			System.out.println("ur not eligible");
+	if(i < 20) {
+		System.out.println("ur teenage");
+	}
+	else {
+		System.out.println("ur grow up");
+		}
+	if ( i > 21) {
+		System.out.println("u r not student");
+	}
+	else if ( i <= 16){
+		System.out.println("u r student");
+	}else {
+		System.out.println(" ur both");
+	}
+	}
+
+}
